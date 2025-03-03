3768c8d8d2bf6004dd6c4673fa7030b62f5edcc7
liyingwei-esp <316449196@qq.com>
2025-03-03 19:54:18 +0800

    V21

--- main/interface/socket/at_socket_task.c
+++ main/interface/socket/at_socket_task.c
@@ -18,7 +18,7 @@
 #include "esp_netif.h"
 #include "nvs_flash.h"
 
-#define AT_SOCKET_RECV_BUFFER_SIZE              256
+#define AT_SOCKET_RECV_BUFFER_SIZE              1024
 #define AT_RING_BUFFER_SIZE                     (8 * 1024)
 
 #ifdef CONFIG_AT_BASE_ON_SOCKET
